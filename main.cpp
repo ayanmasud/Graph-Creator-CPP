a249abbe1208423ba8948a2ce2643bf139787f75
ayan masud <120285056+ayanmasud@users.noreply.github.com>
2025-05-23 13:15:38 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ int main() {
   cout << "'DELV' to delete a vertex" << endl;
   cout << "'DELE' to delete an edge" << endl;
   cout << "'PRINT' to print the graph" << endl;
+  cout << "'FSP' to find shortest path" << endl;
   cout << "'QUIT' to leave" << endl;
 
   while (true) {
@@ -66,13 +67,52 @@ int main() {
       graph[sindex][eindex] = weight;
     }
     else if (strcmp(cmd, "DELV") == 0) { // delete vertex command
+      char vlabel;
+      cout << "What vertex label? ";
+      cin >> vlabel;
+      int index = 0;
+      while (vertices[index] != vlabel) {
+        index++;
+      }
 
+      for (int i = 0; i < 20; i++) { // clear any edge connected to this vertex
+	if (vertices[i] != ' ') {
+	  for (int j = 0; j < 20; j++) {
+	    if (graph[i][j] != -1) {
+	      if (i == index || j == index) {
+		graph[i][j] = -1;
+	      }
+	    }
+	  }
+	}
+      }
+      vertices[index] = ' '; // remove the vertex
     }
     else if (strcmp(cmd, "DELE") == 0) { // delete edge command
+      cout << "Starting node: ";
+      char vstart;
+      cin >> vstart;
+      cout << "Ending node: ";
+      char vend;
+      cin >> vend;
 
+      int sindex = 0;
+      while (vertices[sindex] != vstart) {
+        sindex++;
+      }
+
+      int eindex = 0;
+      while (vertices[eindex] != vend) {
+        eindex++;
+      }
+
+      graph[sindex][eindex] = -1; // remove that edge
     }
     else if (strcmp(cmd, "PRINT") == 0) { // print command
       print(graph, vertices);
+    }
+    else if (strcmp(cmd, "FSP") == 0) { // find shortest path command
+      
     }
     else if (strcmp(cmd, "QUIT") == 0) { // quit command
       break;
@@ -85,19 +125,23 @@ int main() {
 void print(int graph[20][20], char vertices[20]) {
   cout << "  ";
   for (int k = 0; k < 20; k++) {
-    cout << vertices[k] << " ";
+    if (vertices[k] != ' ') {
+      cout << vertices[k] << " ";
+    }
   }
   cout << endl;
   for (int i = 0; i < 20; i++) {
-    cout << vertices[i] << " ";
-    for (int j = 0; j < 20; j++) {
-      if (graph[i][j] == -1) {
-	cout << "  ";
+    if (vertices[i] != ' ') {
+      cout << vertices[i] << " ";
+      for (int j = 0; j < 20; j++) {
+	if (graph[i][j] == -1) {
+	  cout << "  ";
+	}
+	else {
+	  cout << graph[i][j] << " ";
+	}
       }
-      else {
-	cout << graph[i][j] << " ";
-      }
+      cout << endl;
     }
-    cout << endl;
   }
 }
